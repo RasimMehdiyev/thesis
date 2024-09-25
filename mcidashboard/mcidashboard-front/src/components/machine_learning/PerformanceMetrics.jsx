e12f088e282809e4343a5bb6219072066c10008b
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2024-09-25 15:38:33 +0200

    Performance metrics in progress

--- mcidashboard/mcidashboard-front/src/components/machine_learning/PerformanceMetrics.jsx
+++ mcidashboard/mcidashboard-front/src/components/machine_learning/PerformanceMetrics.jsx
@@ -1,10 +1,12 @@
-const PerformanceMetrics= () => {
 
-    return (
-        <div className="card performance-metrics">
-            <p className="ml-subtitle">Performance metrics</p>
-            
-        </div>
-    )
-    }
-    export default PerformanceMetrics
+
+const PerformanceMetrics = () => {
+  return (
+    <div className="card performance-metrics" >
+      <p className="ml-subtitle" style={{paddingBottom:'20px'}}>Performance metrics</p>
+      <img src="/assets/confusion matrix.png"alt="Performance Metrics" style={{ width: '100%', height: 'auto' }} />
+    </div>
+  );
+};
+
+export default PerformanceMetrics;
