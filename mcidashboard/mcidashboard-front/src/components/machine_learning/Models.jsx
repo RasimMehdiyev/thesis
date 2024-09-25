fd53b7fd470ce7f9a6c795fde385c6768060d589
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2024-09-25 13:50:09 +0200

    Pre-processing section done

--- mcidashboard/mcidashboard-front/src/components/machine_learning/Models.jsx
+++ mcidashboard/mcidashboard-front/src/components/machine_learning/Models.jsx
@@ -2,17 +2,17 @@ const Models = () => {
     return (
       <div className="card models">
         <p className="ml-subtitle">Models</p>
-        <p> Number of trained models: <strong>19</strong></p>
+        <p style={{color: '#585858'}}> Number of trained models: <strong style={{color: 'black'}}>19</strong></p>
         <table className="models-table">
           <tbody>
             <tr>
-              <td> <p> <strong>Best performance models:</strong></p> </td>
+              <td> <p> <strong style={{color: 'black'}}>Best performance models:</strong></p> </td>
               <td className="vertical-line-left"></td>
-              <td className="centered" style={{ padding: '10px' }}> <p> <strong>Accuracy</strong></p> </td>
+              <td className="centered" style={{ padding: '10px' }}> <p> <strong style={{color: 'black'}}>Accuracy</strong></p> </td>
             </tr>  
             <tr>
               <td>
-                <ul className="no-margin" style={{ paddingRight: '20px' }}> {/* Add padding-right here */}
+                <ul className="no-margin " style={{ paddingRight: '20px' }}> {/* Add padding-right here */}
                   <li>Nu-Support Vector</li>
                 </ul>
               </td>
