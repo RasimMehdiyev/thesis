848564b7e15d6100f2bf2d206ba834ac3244bba0
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2024-09-25 13:42:24 +0200

    Pre-processing section done

--- mcidashboard/mcidashboard-front/src/components/machine_learning/Models.jsx
+++ mcidashboard/mcidashboard-front/src/components/machine_learning/Models.jsx
@@ -4,38 +4,40 @@ const Models = () => {
         <p className="ml-subtitle">Models</p>
         <p> Number of trained models: <strong>19</strong></p>
         <table className="models-table">
-          <tr>
-            <td> <p> <strong>Best performance models:</strong></p> </td>
-            <td className="vertical-line-left"></td>
-            <td className="centered" style={{padding: '10px'}}> <p> <strong>Accuracy</strong></p> </td>
-          </tr>  
-          <tr>
-            <td>
-              <ul className="no-margin">
-                <li>Nu-Support Vector</li>
-              </ul>
-            </td>
-            <td className="vertical-line-left"></td>
-            <td className="centered"> <strong>83.3% </strong></td>
-          </tr>
-          <tr>
-            <td>
-              <ul className="no-margin">
-                <li>Gradient Boosting Classifier</li>
-              </ul>
-            </td>
-            <td className="vertical-line-left"></td>
-            <td className="centered"> 80.6% </td>
-          </tr>
-          <tr>
-            <td>
-              <ul className="no-margin">
-                <li>Extra Trees Classifier</li>
-              </ul>
-            </td>
-            <td className="vertical-line-left"></td>
-            <td className="centered"> 80.6% </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td> <p> <strong>Best performance models:</strong></p> </td>
+              <td className="vertical-line-left"></td>
+              <td className="centered" style={{ padding: '10px' }}> <p> <strong>Accuracy</strong></p> </td>
+            </tr>  
+            <tr>
+              <td>
+                <ul className="no-margin" style={{ paddingRight: '20px' }}> {/* Add padding-right here */}
+                  <li>Nu-Support Vector</li>
+                </ul>
+              </td>
+              <td className="vertical-line-left"></td>
+              <td className="centered"> <strong>83.3% </strong></td>
+            </tr>
+            <tr>
+              <td>
+                <ul className="no-margin" style={{ paddingRight: '20px' }}> {/* Add padding-right here */}
+                  <li>Gradient Boosting Classifier</li>
+                </ul>
+              </td>
+              <td className="vertical-line-left"></td>
+              <td className="centered"> 80.6% </td>
+            </tr>
+            <tr>
+              <td>
+                <ul className="no-margin" style={{ paddingRight: '20px' }}> {/* Add padding-right here */}
+                  <li>Extra Trees Classifier</li>
+                </ul>
+              </td>
+              <td className="vertical-line-left"></td>
+              <td className="centered"> 80.6% </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
