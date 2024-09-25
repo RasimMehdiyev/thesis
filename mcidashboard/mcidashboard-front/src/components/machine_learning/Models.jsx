2904dd2017ad488dd366a6c071eeea4f88796b6a
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2024-09-25 12:51:35 +0200

    Models section done

--- mcidashboard/mcidashboard-front/src/components/machine_learning/Models.jsx
+++ mcidashboard/mcidashboard-front/src/components/machine_learning/Models.jsx
@@ -1,10 +1,44 @@
-const Models= () => {
-
+const Models = () => {
     return (
-        <div className="card models">
-            <p className="ml-subtitle">Models</p>
-            
-        </div>
-    )
-    }
-    export default Models
+      <div className="card models">
+        <p className="ml-subtitle">Models</p>
+        <p> Number of trained models: <strong>19</strong></p>
+        <table className="models-table">
+          <tr>
+            <td> <p> <strong>Best performance models:</strong></p> </td>
+            <td className="vertical-line-left"></td>
+            <td className="centered" style={{padding: '10px'}}> <p> <strong>Accuracy</strong></p> </td>
+          </tr>  
+          <tr>
+            <td>
+              <ul>
+                <li>Nu-Support Vector</li>
+              </ul>
+            </td>
+            <td className="vertical-line-left"></td>
+            <td className="centered"> 83.3% </td>
+          </tr>
+          <tr>
+            <td>
+              <ul>
+                <li>Gradient Boosting Classifier</li>
+              </ul>
+            </td>
+            <td className="vertical-line-left"></td>
+            <td className="centered"> 80.6% </td>
+          </tr>
+          <tr>
+            <td>
+              <ul>
+                <li>Extra Trees Classifier</li>
+              </ul>
+            </td>
+            <td className="vertical-line-left"></td>
+            <td className="centered"> 80.6% </td>
+          </tr>
+        </table>
+      </div>
+    );
+  };
+  
+  export default Models;
