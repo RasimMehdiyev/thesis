5377dd6d2760feef5d1f0e5159ed3005de085409
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2024-09-29 18:54:47 +0200

    Peformance metrics updated w custom confusion matrix

--- mcidashboard/mcidashboard-front/src/components/machine_learning/PerformanceMetrics.jsx
+++ mcidashboard/mcidashboard-front/src/components/machine_learning/PerformanceMetrics.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import ConfusionMatrix from './ConfusionMatrix';
 
 const PerformanceMetrics = () => {
   const [tooltip, setTooltip] = useState({ visible: false, x: 0, y: 0, text: '' });
@@ -31,119 +32,17 @@ const PerformanceMetrics = () => {
     setTooltip({ visible: false, x: 0, y: 0, text: '' });
   };
 
+
   return (
     <div className="card performance-metrics" style={{ position: 'relative' }}>
       <p className="ml-subtitle" style={{ paddingBottom: '20px' }}>
         Performance metrics
       </p>
-      <img
-        src="/assets/confusion matrix.png"
-        alt="Performance Metrics"
-        style={{ width: '100%', height: 'auto' }}
-      />
-      {/* Bottom Left Hover Area */}
-      <div
-        className="hover-area"
-        style={{
-          position: 'absolute',
-          top: '315px',
-          left: '130px',
-          width: '28%',
-          height: '9%',
-          //For debugging
-          //backgroundColor: 'rgba(255, 0, 0, 0.3)', 
-          //border: '1px solid red', 
-        }}
-        onMouseEnter={(e) =>
-          handleMouseEnter(
-            e,
-            'The bottom-left cell shows the number of MCI patients that were <strong>incorrectly</strong> predicted as being healthy by the model.'
-          )
-        }
-        onMouseLeave={handleMouseLeave}
-      ></div>
 
-      {/* Bottom Right Hover Area */}
-      <div
-        className="hover-area"
-        style={{
-          position: 'absolute',
-          top: '22.5%',
-          left: '52%',
-          width: '29%',
-          height: '9%',
-          //For debugging
-          //backgroundColor: '#00FF007F',
-          //border: '1px solid red', 
-        }}
-        onMouseEnter={(e) =>
-          handleMouseEnter(
-            e,
-            'The bottom-right cell shows the number of MCI that were <strong>correctly</strong> predicted as being MCI patients by the model.'
-          )
-        }
-        onMouseLeave={handleMouseLeave}
-      ></div>
-
-      {/* Top Right Hover Area */}
-      <div
-        className="hover-area"
-        style={{
-          position: 'absolute',
-          top: '13%',
-          left: '52%',
-          width: '29%',
-          height: '9%',
-          //For debugging
-          //backgroundColor: 'rgba(255, 0, 0, 0.3)', 
-          //border: '1px solid red', 
-        }}
-        onMouseEnter={(e) =>
-          handleMouseEnter(e, 'The top-right cell shows the number of healthy participants that were <strong>incorrectly</strong> predicted as being MCI patients by the model.')
-        }
-        onMouseLeave={handleMouseLeave}
-      ></div>
-
-      {/* Top Left Hover Area */}
-      <div
-        className="hover-area"
-        style={{
-          position: 'absolute',
-          top: '13%',
-          left: '23%',
-          width: '29%',
-          height: '9%',
-          //For debugging
-          //backgroundColor: '#00FF007F', 
-          //border: '1px solid red', 
-        }}
-        onMouseEnter={(e) =>
-          handleMouseEnter(e, 'The top-left cell shows the number of healthy participants that were <strong>correctly</strong> predicted as being healthy by the model.')
-        }
-        onMouseLeave={handleMouseLeave}
-      ></div>
-
-      {/* Custom Tooltip */}
-      {/* Now the tooltip won't display if the window width is below 1500px */}
-      {tooltip.visible && windowWidth >= 1500 && (
-        <div
-          className="custom-tooltip"
-          style={{
-            position: 'absolute',
-            top: `${tooltip.y + 300}px`,
-            left: `${tooltip.x-100}px`,
-            backgroundColor: 'white',
-            width: '200px',
-            color: 'black',
-            padding: '10px',
-            borderRadius: '15px',
-            boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.15)', 
-            transform: 'translate(0, -100%)', // Position the tooltip above the mouse
-            zIndex: 10, // Make sure tooltip stays above other content
-          }}
-          dangerouslySetInnerHTML={{ __html: tooltip.text }} // Renders HTML for bolding "incorrectly"
-        ></div>
-      )}
+      <ConfusionMatrix data={[ [13, 5],  [2, 16], ]} model={"Gradient Boosting classifier"}/>
+      <ConfusionMatrix data={[ [13, 5],  [2, 16], ]} model={"Nu-support Vector classifier"}/>
+      <ConfusionMatrix data={[ [13, 5],  [2, 16], ]} model={"Extra Trees classifier"}/>
+    
     </div>
   );
 };
