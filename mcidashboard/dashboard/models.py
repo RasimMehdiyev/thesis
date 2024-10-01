1804282f8f77fceca07cd137a4924de544782898
Rasim Mehdiyev <rasim.mehdiyev@student.kuleuven.be>
2024-10-01 12:39:58 +0200

    udpate

--- mcidashboard/dashboard/models.py
+++ mcidashboard/dashboard/models.py
@@ -73,6 +73,7 @@ class Biomarker(models.Model):
     name = models.CharField(max_length=45)
     type = models.ForeignKey(BiomarkerType, on_delete=models.CASCADE)
     unit = models.CharField(max_length=45, null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.name
 
