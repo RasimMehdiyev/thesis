377da023a6f1f07cc7d4f87e0ca2f119ed1367fb
Rasim Mehdiyev <rasim.mehdiyev@student.kuleuven.be>
2024-12-07 15:16:46 +0100

    removed completion path

--- mcidashboard/mcidashboard-front/src/components/Questionnaire.jsx
+++ mcidashboard/mcidashboard-front/src/components/Questionnaire.jsx
@@ -459,8 +459,9 @@ const handleSendMessage = (answer = message, skip = false) => {
         link.target = '_blank';
         
         sendSystemMessage(
-          `Thank you for your participation!\nPlease copy and paste the following link in another tab to confirm the completion of the study:\n${link.href}`
+          `Thank you for your participation!`
         );
+        // \nPlease copy and paste the following link in another tab to confirm the completion of the study:\n${link.href}
         submitSection(fetchedSections[currentSectionIndex].questions[currentQuestionIndex].id);
       }
     }
